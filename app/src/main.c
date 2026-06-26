dee8be1eb951ed45be36cfa0f9ed3d0c0713b769
Oskar Forstnaric <oskar.forstnaric@gmail.com>
2026-06-26 15:42:48 +0200

    feat(bluetooth): add BLE advertising support and initialize Bluetooth in main

--- app/src/main.c
+++ app/src/main.c
@@ -275,8 +275,7 @@ int main(void)
 	sensor_ug_to_ms2(FLIGHT_WAKEUP_THRESHOLD_UG, &threshold);
 
 	k_mutex_lock(&sensor_mutex, K_FOREVER);
-	rc = sensor_attr_set(prv_acc, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_UPPER_THRESH,
-				 &threshold);
+	rc = sensor_attr_set(prv_acc, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_UPPER_THRESH, &threshold);
 	if (rc != 0) {
 		LOG_ERR("Failed to set wakeup threshold: %d", rc);
 	}
